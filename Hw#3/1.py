ea90bef44022117dad8887bc9ff03e94a6693ed1
NikolayF <mmfolnd@gmail.com>
2021-11-25 22:31:42 +0300

    HW3
    HW3

---
+++ Hw#3/1.py
@@ -0,0 +1,17 @@
+user_numbers = ('первое число', 'второе число')
+numbers_mass = []
+def divide_func(first_num, second_num):
+    try:
+        first_num = int(first_num)
+        second_num = int(second_num)
+        if second_num == 0:
+            print('Делить на 0 нельзя!')
+        else:
+            print(f'{first_num} / {second_num} = {first_num / second_num}')
+    except ValueError:
+        print('Нужно ввести число!')
+
+for user_number in user_numbers:
+    numbers_mass.append(input(f'Введите {user_number} - '))
+
+divide_func(numbers_mass[0], numbers_mass[1])
